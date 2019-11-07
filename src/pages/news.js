8e1cab15d9b6d71012e674a424e57def5a2e7cdb
oNtAiR <39558198+oNtAiR@users.noreply.github.com>
2019-11-07 15:07:00 -0500

    fixed empty file

---
+++ src/pages/news.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+
+const News = () => {
+    return (
+        <Layout>
+        <p>test</p>
+    </Layout>
+    )
+}
+    
+    
+
+export default News
