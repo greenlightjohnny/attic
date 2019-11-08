a6d506f697f17960f79fded5a2d4e7a2b9d0f7e2
oNtAiR <39558198+oNtAiR@users.noreply.github.com>
2019-11-08 15:41:31 -0500

    tt

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Layout from '../components/layout'
 import aboutStyles from './about.module.scss'
+import { Link, useStaticQuery, graphql } from "gatsby"
 
 
 const About = () => {
