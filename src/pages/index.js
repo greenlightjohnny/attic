277e9a3f14f66d7f18f547a39b9afc725f93ba7e
oNtAiR <39558198+oNtAiR@users.noreply.github.com>
2019-11-08 14:50:40 -0500

    1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,134 +1,140 @@
 import React from "react"
 import Layout from "../components/layout"
-import { Link, useStaticQuery, graphql } from 'gatsby'
-import indexStyles from './index.module.scss'
-
-
-
-
+import { Link, useStaticQuery, graphql } from "gatsby"
+import indexStyles from "./index.module.scss"
 
 const MainPage = () => {
-
-      const data = useStaticQuery(graphql`
-      query {
-        allMarkdownRemark {
-          edges {
-            node {
-              frontmatter {
-                title
-                intro
-                introv
-                titlev
-                
-              }
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              intro
+              introv
+              titlev
+              titled
+              introd
+              titleh
+              introh
+              titlef
+              introf
             }
           }
         }
       }
-      `)
+    }
+  `)
 
-     
+  return (
+    <Layout>
+      <div className={indexStyles.background}>
+        <div className={indexStyles.center}>
+          <h2>
+            <span>Angels </span>in The Attic
+          </h2>
+          <p>Floyd, VA</p>
+        </div>
+      </div>
 
-    return (
-        <Layout>
-            <div className={indexStyles.background}>
-                <div className={indexStyles.center}>
-            <h2>
-                    <span>Angels </span>in The Attic
-                </h2>
-                <p>Floyd, VA</p>
-                </div>
-                
-            </div>
+      <section className={indexStyles.flex}>
+        <article>
+          {data.allMarkdownRemark.edges.map((edge, i) => {
+            return (
+              <div key={i}>
+                <h3>{edge.node.frontmatter.title}</h3>
+                <p>{edge.node.frontmatter.intro}</p>
+              </div>
+            )
+          })}
 
-           
-            
+          <div className={indexStyles.morec}>
+            <Link className={indexStyles.more} to="/about">
+              Read More
+            </Link>
+          </div>
+        </article>
+        <article>
+          <h3>News</h3>
+          <div
+            className="fb-page"
+            data-href="https://www.facebook.com/AngelsintheAtticFloydVa/"
+            data-width="380"
+            data-hide-cover="false"
+            data-show-facepile="false"
+          ></div>
+          <div className={indexStyles.morec}>
+            <Link className={indexStyles.more} to="/about">
+              Read More
+            </Link>
+          </div>
+        </article>
+        <article>
+          {data.allMarkdownRemark.edges.map((edge, i) => {
+            return (
+              <div key={i}>
+                <h3>{edge.node.frontmatter.titlev}</h3>
+                <p>{edge.node.frontmatter.introv}</p>
+              </div>
+            )
+          })}
 
-            <section className={indexStyles.flex}>
-                <article>
-                    {data.allMarkdownRemark.edges.map((edge, i) => {
-                        return (
-                        <div key={i}>
-                          <h3>{edge.node.frontmatter.title}</h3>
-                          <p>{edge.node.frontmatter.intro}</p> 
-                          </div>
-                    )
-                    })}
-                    
-                    <div className={indexStyles.morec}>
-                    <Link className={indexStyles.more} to="/about">Read More</Link>
-                    </div>
-                    
-                </article>
-                <article>
-                    <h3>News</h3>
-                    <div className="fb-page" 
-  data-href="https://www.facebook.com/AngelsintheAtticFloydVa/"
-  data-width="380" 
-  data-hide-cover="false"
-  data-show-facepile="false"></div>
-                    <div className={indexStyles.morec}>
-                    <Link className={indexStyles.more} to="/about">Read More</Link>
-                    </div>
-                </article>
-                <article>
-                    {data.allMarkdownRemark.edges.map((edge, i) => {
-                        return (
-                            <div key={i}>
-                                <h3>{edge.node.frontmatter.titlev}</h3>
-                                <p>{edge.node.frontmatter.introv}</p>
-                            </div>
-                        )
-                    })}
-                
-                        <div className={indexStyles.morec}>
-                        <Link className={indexStyles.more} to="/about">Read More</Link>
-                        </div>
-                </article>
-                <article>
-                {data.allMarkdownRemark.edges.map((edge, i) => {
-                return (
-                            <div key={i}>
-                                <h3>{edge.node.frontmatter.title}</h3>
-                                <p>{edge.node.frontmatter.intro}</p>
-                            </div>
-                        )
-                    })}
+          <div className={indexStyles.morec}>
+            <Link className={indexStyles.more} to="/about">
+              Read More
+            </Link>
+          </div>
+        </article>
+        <article>
+          {data.allMarkdownRemark.edges.map((edge, i) => {
+            return (
+              <div key={i}>
+                <h3>{edge.node.frontmatter.titled}</h3>
+                <p>{edge.node.frontmatter.introd}</p>
+              </div>
+            )
+          })}
 
-                    <div className={indexStyles.morec}>
-                    <Link className={indexStyles.more} to="/about">Read More</Link>
-                    </div>
-                </article>
-                <article>
-                {data.allMarkdownRemark.edges.map((edge, i) => {
-                return (
-                            <div key={i}>
-                                <h3>{edge.node.frontmatter.title}</h3>
-                                <p>{edge.node.frontmatter.intro}</p>
-                            </div>
-                        )
-                    })}
-                    <div className={indexStyles.morec}>
-                    <Link className={indexStyles.more} to="/about">Read More</Link>
-                    </div>
-                </article>
-                <article>
-                {data.allMarkdownRemark.edges.map((edge, i) => {
-                return (
-                            <div key={i}>
-                                <h3>{edge.node.frontmatter.title}</h3>
-                                <p>{edge.node.frontmatter.intro}</p>
-                            </div>
-                        )
-                    })}
-                    <div className={indexStyles.morec}>
-                    <Link className={indexStyles.more} to="/about">Read More</Link>
-                    </div>
-                </article>
-
-            </section>
-        </Layout>
-    )
+          <div className={indexStyles.morec}>
+            <Link className={indexStyles.more} to="/about">
+              Read More
+            </Link>
+          </div>
+        </article>
+        <article>
+          {data.allMarkdownRemark.edges.map((edge, i) => {
+            return (
+              <div key={i}>
+                <h3>{edge.node.frontmatter.titleh}</h3>
+                <p>{edge.node.frontmatter.introh}</p>
+              </div>
+            )
+          })}
+          <div className={indexStyles.morec}>
+            <Link className={indexStyles.more} to="/about">
+              Read More
+            </Link>
+          </div>
+        </article>
+        <article>
+          {data.allMarkdownRemark.edges.map((edge, i) => {
+            return (
+              <div key={i}>
+                <h3>{edge.node.frontmatter.titlef}</h3>
+                <p>{edge.node.frontmatter.introf}</p>
+              </div>
+            )
+          })}
+          <div className={indexStyles.morec}>
+            <Link className={indexStyles.more} to="/about">
+              Read More
+            </Link>
+          </div>
+        </article>
+      </section>
+    </Layout>
+  )
 }
 
 export default MainPage
