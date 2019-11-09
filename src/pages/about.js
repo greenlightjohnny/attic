445eb568c3439273cc2aed893d5a816b01550775
oNtAiR <39558198+oNtAiR@users.noreply.github.com>
2019-11-08 21:37:11 -0500

    markdown

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,7 +13,12 @@ const About = () => {
             node {
               frontmatter {
                 title
-                intro
+                history
+                historytext
+                donations
+                donationstext
+                creed
+                creedtext
               }
             }
           }
@@ -28,12 +33,22 @@ const About = () => {
 
             <section className={aboutStyles.container}>
 
-                {data.allMarkdownRemark.edges.map((edge) => {
+                {data.allMarkdownRemark.edges.map((edge, index) => {
                     return (
-                        <h1>{edge.node.frontmatter.title}</h1>
+                        <h1 key={index}>{edge.node.frontmatter.title}</h1>
                     )
                 })}
                 <h1>About</h1>
+
+                {data.allMarkdownRemark.edges.map((edge, index) => {
+                    return (
+                        <article key={index} className={aboutStyles.section}>
+                            <h2>{edge.node.frontmatter.history}</h2>
+                            <p>{edge.node.frontmatter.historytext}</p>
+                        </article>
+                        
+                        )
+                })}
             <article className={aboutStyles.section}>
                 <h2>Our History</h2>
                 <p>Angels in the Attic began with a seed of an idea from its founder Kathy Blackwell. Kathy provided countless hours over the years she served as director, making Angels a true ministry while helping the people of Floyd County. Opening in March of 2000, Angels in the Attic operated in two different locations before finding the “forever home” at 201 South Locust Street in 2002. This building, near the center of the Town of Floyd, Virginia, was paid off in 14 months with fundraisers and community support.
@@ -43,15 +58,27 @@ Mid 2011, the Angels Boutique was opened. This adjacent retail space was created
 Also in 2011, a house with outbuildings and acreage was purchased that was located only a quarter mile away from downtown at 112 Needmore Lane in Floyd. This property is used as the Donation Center. This facility is convenient for the community to drop off their donated goods and is open seven days a week. Volunteers at the donation center sort, box, label and deliver donations to the stores.</p>
             </article>
 
-            <article className={aboutStyles.section}>
-                <h2>Honoring Donations</h2>
-                <p>
-                Angels is committed to honoring the gift of each donation by restocking gently used, like new and new items each day of operation. Every effort is made to ensure donations are first available for sale in Angels in the Attic stores and then shared with other not-for-profit human service agencies such as the Rescue Mission, Salvation Army and Goodwill. 
-                </p>
+            {data.allMarkdownRemark.edges.map((edge, index) => {
+                    return (
+                        <article key={index} className={aboutStyles.section}>
+                            <h2>{edge.node.frontmatter.donations}</h2>
+                            <p>{edge.node.frontmatter.donationstext}</p>
+                        </article>
+                        
+                        )
+                })}
 
-            </article>
 
 
+{data.allMarkdownRemark.edges.map((edge, index) => {
+                    return (
+                        <article key={index} className={aboutStyles.section}>
+                            <h2>{edge.node.frontmatter.creed}</h2>
+                            <p>{edge.node.frontmatter.creedtext}</p>
+                        </article>
+                        
+                        )
+                })}
             <article className={aboutStyles.ending}>
                 <h2>Angels in the Attic Creed:</h2>
                 <p><span>
