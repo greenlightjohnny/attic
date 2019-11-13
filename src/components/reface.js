0f8bd052ac3b4630769bc7cdd23faf34a24d1270
oNtAiR <39558198+oNtAiR@users.noreply.github.com>
2019-11-12 19:30:16 -0500

    sdk removal

--- src/components/reface.js
+++ src/components/reface.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-const { FacebookProvider, Page } = require('react-facebook')
+// import React, { Component } from 'react';
+// const { FacebookProvider, Page } = require('react-facebook')
  
-export default class Example extends Component {
+// export default class Example extends Component {
 
-    componentDidMount(){
-        window.FB.XFBML.parse();
+//     componentDidMount(){
+//         window.FB.XFBML.parse();
 
-     }
-  render() {
-    return (
-      <FacebookProvider appId="2518108111807119">
-        <Page href="https://www.facebook.com/AngelsintheAtticFloydVa/" className tabs="timeline" />
-      </FacebookProvider>    
-    )
-  }
-}
+//      }
+//   render() {
+//     return (
+//       <FacebookProvider appId="2518108111807119">
+//         <Page href="https://www.facebook.com/AngelsintheAtticFloydVa/" className tabs="timeline" />
+//       </FacebookProvider>    
+//     )
+//   }
+// }
