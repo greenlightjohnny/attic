eb50e875598cdf4b631e2314350b6b71047e50f5
oNtAiR <39558198+oNtAiR@users.noreply.github.com>
2019-11-16 12:03:57 -0500

    hmmm

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,19 +36,21 @@ module.exports = {
       },
     },
 
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "src",
-        path: `${__dirname}/src/`, 
-      },
-    },
+  
 
     {
       resolve: 'gatsby-source-contentful',
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`, 
       },
     },
 
