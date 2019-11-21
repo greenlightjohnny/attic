a79b41468d0e3164215a24c58c0ca58728756afb
oNtAiR <39558198+oNtAiR@users.noreply.github.com>
2019-11-21 13:05:09 -0500

    gg

--- src/pages/legal.js
+++ src/pages/legal.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/SEO'
-import aboutStyles from './about/module.scss'
+import aboutStyles from './about.module.scss'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import { graphql } from 'gatsby'
 export const query = graphql`
