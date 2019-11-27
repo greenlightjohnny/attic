d98459e86e44b08d21c78d3fff7b46cd8b27da3c
oNtAiR <39558198+oNtAiR@users.noreply.github.com>
2019-11-27 15:09:00 -0500

    CSP config changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,7 @@ module.exports = {
         mergeStyleHashes: true, // you can disable styles sha256 hashes
         mergeDefaultDirectives: true,
         directives: {
-          "script-src": "'self' www.facebook.com",
+          "script-src": "'self' 'www.facebook.com' ''https://connect.facebook.net/en_US/sdk.js'",
           "style-src": "'self' 'unsafe-inline'",
           //"img-src": "'self' data: www.google-analytics.com"
           // you can add your directives or override defaults
