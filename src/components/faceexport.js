4b060ce1b95dc2f6ee855fe72c1b65371d9892f7
oNtAiR <39558198+oNtAiR@users.noreply.github.com>
2019-12-04 11:37:53 -0500

    Changed fb width again

--- src/components/faceexport.js
+++ src/components/faceexport.js
@@ -15,7 +15,7 @@ export default class Faceex extends Component {
        
        
         
-        <div className="fb-page" data-href="https://www.facebook.com/AngelsintheAtticFloydVa/" data-tabs="timeline" margin="0 auto" data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/AngelsintheAtticFloydVa/" className="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/AngelsintheAtticFloydVa/">Angels Facebook</a></blockquote></div>
+        <div className="fb-page" data-href="https://www.facebook.com/AngelsintheAtticFloydVa/" data-tabs="timeline" margin="0 auto" data-width="500" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/AngelsintheAtticFloydVa/" className="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/AngelsintheAtticFloydVa/">Angels Facebook</a></blockquote></div>
         
     
         
