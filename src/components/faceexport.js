805e2248c63cbc324a2d7e59c562c1910f466957
oNtAiR <39558198+oNtAiR@users.noreply.github.com>
2019-12-04 14:54:59 -0500

    F facebook

--- src/components/faceexport.js
+++ src/components/faceexport.js
@@ -15,7 +15,7 @@ export default class Faceex extends Component {
        
        
         
-        <div className="fb-page" data-href="https://www.facebook.com/AngelsintheAtticFloydVa/" data-tabs="timeline"   data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/AngelsintheAtticFloydVa/" className="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/AngelsintheAtticFloydVa/">Angels Facebook</a></blockquote></div>
+        <div className="fb-page" data-href="https://www.facebook.com/AngelsintheAtticFloydVa/" data-tabs="timeline"   data-height="" data-width="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/AngelsintheAtticFloydVa/" className="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/AngelsintheAtticFloydVa/">Angels Facebook</a></blockquote></div>
         
     
         
